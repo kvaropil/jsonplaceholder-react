12158e0cb664bb4b8c8bbb8b5a62e8e9ea6db5c1
Roman Kvapil <kvapilrom@gmail.com>
2023-08-29 13:21:15 +0200

    feat: Implement PostDetail as modal

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -3,6 +3,7 @@ import { Post } from '../types';
 import { getPosts } from '../api/posts';
 import styled from 'styled-components';
 import { shortenText } from '../utils';
+import { PostDetail } from './PostDetail';
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.colors.grey};
@@ -27,10 +28,34 @@ const Body = styled.p`
   color: ${(props) => props.theme.colors.black};
 `;
 
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3); // semi-transparent black
+  backdrop-filter: blur(5px); // Apply blur effect
+  display: flex;
+  align-items: center;
+  justify-content: center; // Childs element (modal) get centered
+  z-index: 10;
+`;
+
+const Modal = styled.div`
+  width: 80%;
+  max-width: 600px;
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3); // Add shadow
+`;
+
 export const PostsList: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -46,6 +71,16 @@ export const PostsList: React.FC = () => {
       });
   }, []);
 
+  const closeModal = () => {
+    setSelectedPost(null);
+  };
+
+  const onOverlayClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -55,17 +90,30 @@ export const PostsList: React.FC = () => {
   }
 
   return (
-    <Container>
-      {posts.length == 0 ? (
-        <p>No posts available</p>
-      ) : (
-        posts.map((post) => (
-          <PostContainer key={post.id}>
-            <Title>{post.title}</Title>
-            <Body>{shortenText(post.body)}</Body>
-          </PostContainer>
-        ))
+    <>
+      <Container>
+        {posts.length == 0 ? (
+          <p>No posts available</p>
+        ) : (
+          posts.map((post) => (
+            <PostContainer key={post.id} onClick={() => setSelectedPost(post)}>
+              <Title>{post.title}</Title>
+              <Body>{shortenText(post.body)}</Body>
+            </PostContainer>
+          ))
+        )}
+      </Container>
+
+      {selectedPost && (
+        <Overlay onClick={onOverlayClick}>
+          <Modal>
+            <PostDetail
+              post={selectedPost}
+              goBack={() => setSelectedPost(null)}
+            />
+          </Modal>
+        </Overlay>
       )}
-    </Container>
+    </>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,7 @@
 export const theme = {
   colors: {
     main: '#00A86B',
+    mainDimmed: '#008A59',
     white: '#FFFFFF',
     black: '#000000',
     grey: '#F1F1F1',
